384c96bf3a73cbb1f753d3c4e408a2f819ae6513
Trọng Kiên <trkien.dev@gmail.com>
2025-12-11 05:32:42 +0700

    Implement ContainsDuplicate method using HashSet

---
+++ Set - HashSet/217_Contains_Duplicate.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        var set = new HashSet<int>();
+        foreach (var n in nums)
+        {
+            if (!set.Add(n)) 
+                return true;
+        }
+        return false;
+    }
+}
